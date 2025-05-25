f4d33af1216ab9f8503d2818b7a258643664b019
Leo6485 <lo570354@gmail.com>
2025-05-25 16:19:30 -0300

    Heurística adicionado na main2.c, improvisado

--- src/main2.c
+++ src/main2.c
@@ -8,8 +8,60 @@
 #include <stdbool.h>
 #include "solve.h"
 
-void heuristica(Graph *g, int max_depth, int capacidade) {
 
+
+int get_max(Graph *g, int capacidade, int viz, int toggle) {
+    int max = -1;
+    int max_razao = 0;
+    for (int i = 1; i < g->num_vertices; i++) {
+        int razao = capacidade / g->towns[i].w * g->towns[i].v;
+        if ((g->data[i][viz] || !toggle) && razao > max_razao) {
+            max_razao = razao;
+            max = i;
+        }
+    }
+
+    return max;
+}
+
+
+void heuristica(Graph *g, int max_depth, int capacidade) {
+    int habilidade = 0;
+
+    int *c = malloc(sizeof(int) * g->num_vertices);
+    int i = get_max(g, capacidade, 0, 0);
+    int prev_i = i;
+    int *solucao = malloc(sizeof(int) * max_depth*2);
+    int j = 0;
+    while (1) {
+        c[i] = capacidade / g->towns[i].w; 
+        habilidade += c[i] * g->towns[i].v;
+        capacidade -= c[i] * g->towns[i].w;
+        g->towns[i].v = 0; // Deleta
+
+        solucao[j] = i+1;
+        solucao[j+1] = c[i];
+
+        max_depth -= g->data[i][prev_i];
+        prev_i = i;
+
+        // Maior vizinho
+        i = get_max(g, capacidade, prev_i, 1);
+        if (i == -1 || capacidade < 0) {
+            break;
+        }
+        j += 2;
+    }
+
+    printf("%d ", habilidade);
+
+    for(int i = 0; i <= j; i += 2) {
+        printf("%d %d ", solucao[i], solucao[i+1]);
+    }
+    printf("\n");
+
+    free(c);
+    free(solucao);
 }
 
 void programacao_dinamica() {
@@ -103,6 +155,10 @@ int main(int argc, char *argv[]){
             add_conn(g, pi - 1, pj - 1, di);
         }
 
+        if (option == 2) {
+            heuristica(g, max_depth, capacidade);
+            continue;
+        }
         Dp *dp = dp_init(max_depth, num_vertices, capacidade);
         calc(dp, g);
         reconstruir(dp);
